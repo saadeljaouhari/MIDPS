bab453bdca21ab95eeea229e9527284d0191a5d6
andutzu7 <rockkidd57@gmail.com>
2023-05-31 22:43:11 +0200

    bugfix

--- utils/server_structure_graph/extract_crawl_data.sh
+++ utils/server_structure_graph/extract_crawl_data.sh
@@ -6,6 +6,6 @@ crawling_agents_ip_regex=$(ifconfig $interface_name  | grep inet | awk '{print $
 log_file_path='/var/log/nginx/access.log'
 
 timestamp=$1
-$output_location=$2
+output_location=$2
 
 awk -v d1="[$timestamp" '($4) >= d1' $log_file_path | grep -wE "$crawling_agents_ip_regex" | grep -w 200 | grep -w HEAD| awk '{printf "%s %s\n", $7, $11}' | sort | uniq | sed -e 's/ /\n/g' | tr -d '"' > $output_location
