4b70e5c5008dfa8dee57fe3651287894dbb364a0
andutzu7 <rockkidd57@gmail.com>
2023-05-31 21:59:14 +0200

    added norm traffic module

--- utils/normal_data_traffic_gen/gen_traffic.sh
+++ utils/normal_data_traffic_gen/gen_traffic.sh
@@ -3,14 +3,14 @@ links_file_path='utils/normal_data_traffic_gen/web_page_links'
 
 download_path='utils/normal_data_traffic_gen/page_output_tmp'
 
-while true
+sleep_duration=2
+
+while IFS= read -r link; do
 do
 	rm -rf $download_path
 	mkdir $download_path
 	selected_link=$(shuf -n 1 $links_file_path)
-	wget -q -p -P $download_path --inet4-only -e robots=off $selected_link
+	wget -q -p -P $download_path --inet4-only -e robots=off $link
 
-
-	sleeping_time=$(shuf -i 5-10 -n 1)
-	sleep $((sleeping_time))
-done
+	sleep $sleep_duration
+done < $links_file_path
