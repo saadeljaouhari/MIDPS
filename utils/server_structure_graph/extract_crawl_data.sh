f78fdf0b26d81ed42fc3ebdb15dcf02a0e7990af
andutzu7 <rockkidd57@gmail.com>
2023-05-31 23:32:22 +0200

    small bugfix🙄

--- utils/server_structure_graph/extract_crawl_data.sh
+++ utils/server_structure_graph/extract_crawl_data.sh
@@ -6,8 +6,7 @@ crawling_agents_ip_regex=$(ifconfig $interface_name  | grep inet | awk '{print $
 log_file_path='/var/log/nginx/access.log'
 
 timestamp=$1
-output_location=$2
-echo "################################"
-echo $output_location
+
+output_location=utils/server_structure_graph/parsed_site_data
 
 awk -v d1="[$timestamp" '($4) >= d1' $log_file_path | grep -wE "$crawling_agents_ip_regex" | grep -w 200 | grep -w HEAD| awk '{printf "%s %s\n", $7, $11}' | sort | uniq | sed -e 's/ /\n/g' | tr -d '"' > $output_location
--- utils/server_structure_graph/fs_gen.sh
+++ utils/server_structure_graph/fs_gen.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 web_structure_tree_path=utils/server_structure_graph/web_structure_tree
-extracted_data_path=utils/server_structure_graph/parsed_site_data
 
 
 rm $web_structure_tree_path
@@ -9,7 +8,6 @@ rm $extracted_data_path
 
 crawl_start_ts=$1
 
-echo $extracted_data_path
-sh utils/server_structure_graph/extract_crawl_data.sh $crawl_start_ts $extracted_data_path
+sh utils/server_structure_graph/extract_crawl_data.sh $crawl_start_ts
 
 python3 utils/server_structure_graph/referrer_correlation.py
