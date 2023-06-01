0c2104ebef110640f8515cc6bc7f7fe70040cfa3
andutzu7 <rockkidd57@gmail.com>
2023-06-01 12:26:48 +0200

    regex bugfix

--- utils/normal_access_pattern_gen/compute_patterns.sh
+++ utils/normal_access_pattern_gen/compute_patterns.sh
@@ -6,7 +6,7 @@ crawl_log_timeframe=1
 
 
 interface_name='enp1s0'
-crawling_agents_ip_regex=$(ifconfig $interface_name  | grep inet6 | awk '{print $2}' | sed -z 's/\n/|/g')
+crawling_agent_ip=$(ifconfig $interface_name  | grep inet6 | head -n 1 |  awk '{print $2}' )
 
 log_file_path='/var/log/nginx/access.log'
 
@@ -20,6 +20,6 @@ folder_timestamp=$(date +%s)
 
 mkdir $extracted_data_path/$folder_timestamp
 
-awk -v d1="[$timestamp" '($4) >= d1' $log_file_path | grep -wE "$crawling_agents_ip_regex" | grep -w 200 | grep -w GET | sort | uniq > $extracted_data_path/$folder_timestamp/normal_access_logs
+awk -v d1="[$timestamp" '($4) >= d1' $log_file_path | grep -wE "$crawling_agent_ip" | grep -w 200 | grep -w GET | sort | uniq > $extracted_data_path/$folder_timestamp/normal_access_logs
 
 python3 modules/normal_traffic_analyzer/analyze.py $extracted_data_path/$folder_timestamp $crawl_log_timeframe
