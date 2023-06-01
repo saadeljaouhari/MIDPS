247ab1a6852daefc579bdf781d91bdb0b5f220d5
andutzu7 <rockkidd57@gmail.com>
2023-06-01 11:24:15 +0200

    syning the ocmputation

--- capture_traffic.sh
+++ capture_traffic.sh
@@ -23,6 +23,8 @@ initialize_modules(){
 
   sh utils/gen_resource_links/gen_resource_links.sh
   # we timestamp the begginning of the operation
+  # we wait one second of sync reasons
+  sleep 1
   start_ts=$(date +"%d/%b/%Y:%H:%M:%S")
   # before starting to analyze traffic we need to crawl the web app at least once
   # we check the existence of the file structure tree file
