fe42565e694a8b1990427d23af5c443c0e31d7b0
andutzu7 <rockkidd57@gmail.com>
2023-06-05 13:08:12 +0200

    bugfixing

--- capture_traffic.sh
+++ capture_traffic.sh
@@ -42,7 +42,7 @@ initialize_modules(){
   # we check the existence of the file structure tree file
   # Run the normal traffic generator agent
   sh ./utils/normal_data_traffic_gen/gen_traffic.sh $output_links_file_path $sleep_time_between_accesses
-  sh ./utils/normal_access_pattern_gen/compute_patterns.sh $crawling_agent_ip $start_ts
+  sh ./utils/normal_access_pattern_gen/compute_patterns.sh $crawling_agent_ip6 $start_ts
 
 }
 cleanup() {
