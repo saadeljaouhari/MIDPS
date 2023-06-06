4bfd4d2b9be94770ae67a7469c3f4406d12ab789
andutzu7 <rockkidd57@gmail.com>
2023-06-06 20:46:49 +0200

    very small bugfix

--- modules/file_disclosure/perform_file_disclosure_detection.sh
+++ modules/file_disclosure/perform_file_disclosure_detection.sh
@@ -3,5 +3,5 @@
 file_path=$1
 address=$(basename $file_path)
 while read line; do
-	echo $line | awk '{printf("%s %s\n",$3,$5)}' | python3 modules/file_disclosure/check.py $address
+	echo $line | python3 modules/file_disclosure/check.py $address
 done < $file_path
