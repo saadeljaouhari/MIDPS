1ef1cb539142137753f407f1119ded65cc11008e
andutzu7 <rockkidd57@gmail.com>
2023-06-06 13:05:48 +0200

    added missing parameter

--- utils/normal_access_pattern_gen/run_pattern_agent.sh
+++ utils/normal_access_pattern_gen/run_pattern_agent.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+# the ip for the extraction
+crawling_agent_ip6=$1
+
 while true
 do
-sh utils/normal_access_pattern_gen/run_compute_access_patterns.sh
+sh utils/normal_access_pattern_gen/run_compute_access_patterns.sh $crawling_agent_ip6
 done
