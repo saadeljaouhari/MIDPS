7e1fac299a2901073b4c151ecea2dfeb237ae412
andutzu7 <rockkidd57@gmail.com>
2023-06-12 18:52:35 +0200

    small dumbo bugfix

--- modules/ddos/perform_dos_detection.sh
+++ modules/ddos/perform_dos_detection.sh
@@ -6,5 +6,5 @@ normal_seq_file='modules/resources/normal_access_patterns'
 ratio_multiplying_factor=5
 #address=$(basename $file_path)
 
-longest_seq_len=$(python3 modules/ddos/compute_longest_seq.sh $normal_seq_file)
+longest_seq_len=$(sh modules/ddos/compute_longest_seq.sh $normal_seq_file)
 python3 modules/ddos/dos_check.py $file_path $norm_traffic_timeframe_size $longest_seq_len $ratio_multiplying_factor
