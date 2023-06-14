8ec2123003c339a2f4efc04ba4c72ecf69fd7105
andutzu7 <rockkidd57@gmail.com>
2023-06-14 17:09:47 +0200

    smol dumbo bugfix

--- utils/ban_suspicious_activities/apply_ban.sh
+++ utils/ban_suspicious_activities/apply_ban.sh
@@ -4,7 +4,7 @@ verdict_folder_path=$1
 
 for file_name in "$verdict_folder_path"/*; do
 
-	ip_address=$file_name
+	ip_address=$(awk '{print $1}' $file_name)
 
 	fail2ban-client set aml-jail banip $ip_address
 
