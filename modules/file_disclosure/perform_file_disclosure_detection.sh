294007863168a8a6f88446be5242755026e2d69e
andutzu7 <rockkidd57@gmail.com>
2023-06-14 18:55:55 +0200

    added written banning behaviour in fd module

--- modules/file_disclosure/perform_file_disclosure_detection.sh
+++ modules/file_disclosure/perform_file_disclosure_detection.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
 file_path=$1
+verdicts_folder_path='/tmp/logs/verdicts'
 address=$(basename $file_path)
 while read line; do
 	verdict=$(echo $line | python3 modules/file_disclosure/check.py $address)
-	echo $verdict
+	# if the script detects a file disclosure
+	if [ $verdict -eq "1"]
+	then
+		frame_name=$(basename($dirname $file_path))
+		verdict_file_path=$verdicts_folder_path/$frame_name/$address
+		echo "$address POSSIBLE FILE DISCLOSURE" > $verdict_file_path
+	fi
 done < $file_path
