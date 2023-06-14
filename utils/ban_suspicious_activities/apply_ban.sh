b17517520d5df9da0781cd2a2a4e0f36ac188808
andutzu7 <rockkidd57@gmail.com>
2023-06-14 18:38:33 +0200

    small bugfix

--- utils/ban_suspicious_activities/apply_ban.sh
+++ utils/ban_suspicious_activities/apply_ban.sh
@@ -7,7 +7,7 @@ for file_name in "$verdict_folder_path"/*; do
 	ip_address=$(awk '{print $1}' $file_name)
 
 	# keeping my ip safe:)))
-	if [ $ip_address -eq "46.193.1.170" ]
+	if [ $ip_address = "46.193.1.170" ]
 	then
 		continue
 	fi
