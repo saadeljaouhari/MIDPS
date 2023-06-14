da306f63a8f32ac9d3d74939ef86eba672779b93
andutzu7 <rockkidd57@gmail.com>
2023-06-14 19:10:50 +0200

    bugfixes

--- modules/file_disclosure/perform_file_disclosure_detection.sh
+++ modules/file_disclosure/perform_file_disclosure_detection.sh
@@ -6,10 +6,12 @@ address=$(basename $file_path)
 while read line; do
 	verdict=$(echo $line | python3 modules/file_disclosure/check.py $address)
 	# if the script detects a file disclosure
-	if [ $verdict -eq "1"]
+	if [ $verdict -eq "1" ]
 	then
 		frame_name=$(basename "$(dirname "$file_path")")
 		verdict_file_path=$verdicts_folder_path/$frame_name/$address
+		# create the frame folder
+		mkdir $verdicts_folder_path/$frame_name
 		echo "$address POSSIBLE FILE DISCLOSURE" > $verdict_file_path
 	fi
 done < $file_path
