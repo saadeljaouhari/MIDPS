2badf552363a136ee61c391c593c8611183bc0ed
andutzu7 <rockkidd57@gmail.com>
2023-06-18 10:05:21 +0200

    smol bugfix

--- utils/ban_suspicious_activities/apply_ban.sh
+++ utils/ban_suspicious_activities/apply_ban.sh
@@ -6,11 +6,6 @@ for file_name in "$verdict_folder_path"/*; do
 
 	ip_address=$(awk '{print $1}' $file_name)
 
-	# keeping my ip safe:)))
-	if [ $ip_address = "46.193.1.170" ]
-	then
-		continue
-	fi
-	fail2ban-client set aml-jail banip $ip_address
+	#fail2ban-client set aml-jail banip $ip_address
 
 done
