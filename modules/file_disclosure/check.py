c3a478b7de8f179cf8c77ed86b2c02eb8437258b
andutzu7 <rockkidd57@gmail.com>
2023-06-22 16:20:15 +0200

    retrained sql model

--- modules/file_disclosure/check.py
+++ modules/file_disclosure/check.py
@@ -18,16 +18,13 @@ def check_file_disclosure(line):
 # in some cases, especially in malware injection the request length is not standard
     if len(line.split(' ')) == 6:
         resource = line.split(' ')[3]
-        referrer = line.split(' ')[5]
 
         result=findall(tree, filter_=lambda node: node.name == resource)
         if len(result)==0:
-            #print('{} attempted a file disclosure. Requested resource: {}'.format(address,resource))
             print("1")
         else:
             print("0")
     else:
-            #print('{} attempted a file disclosure. The request has an irregular format'.format(address))
             print("1")
 
 if __name__=="__main__":
